8c096052d01cbb31d3bf1716c2178878d95722bb
caritope2804 <caritope2804@gmail.com>
2022-08-18 23:01:19 -0300

    agrego autenticacion google

--- js/index.js
+++ js/index.js
@@ -17,4 +17,11 @@ if (sessionStorage.getItem("loggedUser")===null || sessionStorage.getItem("logge
 
     location.replace("login.html");
 
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+      }
+
     
