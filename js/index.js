f6a3c07f58d36f5ea58cc83eb26d6e682005e1e4
caritope2804 <caritope2804@gmail.com>
2022-08-19 00:13:01 -0300

    agrego autenticacion google

--- js/index.js
+++ js/index.js
@@ -17,11 +17,6 @@ if (sessionStorage.getItem("loggedUser")===null || sessionStorage.getItem("logge
 
     location.replace("login.html");
 
-    function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-          console.log('User signed out.');
-        });
-      }
+    
 
     
--- js/login.js
+++ js/login.js
@@ -10,11 +10,3 @@ sessionStorage.setItem("loggedUser",true);
 }
 sessionStorage.setItem("loggedUser",false);
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    location.replace("index.html");
-  }
